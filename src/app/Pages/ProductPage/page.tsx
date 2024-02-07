57b4025e42699224c19056afaddd0e24dd8a73fb
Aryan Chouhan <97756708+aryan-c02@users.noreply.github.com>
2024-02-07 20:33:07 +0530

    minor-improvements

--- src/app/Pages/ProductPage/page.tsx
+++ src/app/Pages/ProductPage/page.tsx
@@ -16,10 +16,10 @@ const ProductPage = () => {
                     <span className="text-[#ABB3BF] font-switzer font-medium leading-6 uppercase text-lg">Discover Products /</span>
                     <span className="text-[#667385] font-switzer font-medium leading-6 uppercase text-lg">&nbsp; Action Camera Ultra HD1080P...</span>
                 </div>
-                <div className="w-24">
-                    <button className="flex bg-white rounded-lg items-center  py-2 border shadow flex pl-2 gap-2 rounded bg-[#FAFAFB] mt-6 w-full h-[40px]">
-                        <div className="w-6">
-                            <Image src="/arrow-right-icon.svg" alt="arrow-icon" width={30} height={30} />
+                <div className="w-24 mt-6">
+                    <button className="flex rounded-lg items-center pl-2 border shadow   gap-1  bg-[#FAFAFB] w-full h-[40px]">
+                        <div className="">
+                            <Image src="/arrow-right-icon.svg" alt="arrow-icon" width={24} height={24} />
                         </div>
                         <div className="color-[#080C10B8] text-base font-switzer font-medium leading-5">Back</div>
                     </button>
